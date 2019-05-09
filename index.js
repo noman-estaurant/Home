c27ceeb7e84759cb083ad36b5f71a78fbc884bf5
emschenn <emilynov14@gmail.com>
2019-05-09 22:13:58 +0800

    98%

---
+++ index.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    $("#menu").hide();
+
+});
+
+
+
+$("#one").click(function() {
+    $(this).animate({
+        backgroundColor: "rgb(255,123,159)",
+        boxShadow: "0px 10px 20px rgba(158, 77, 100, 0.5)",
+    }, 150);
+});
+$("#many").click(function() {
+    $(this).animate({
+        backgroundColor: "rgb(255,123,159)",
+        boxShadow: "0px 10px 20px rgba(158, 77, 100, 0.5)",
+    }, 150);
+});
